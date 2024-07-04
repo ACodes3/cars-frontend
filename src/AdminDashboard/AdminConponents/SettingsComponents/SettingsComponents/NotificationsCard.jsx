621a97e3afd1da0c05d233904a344ea7d3f3dfea
Aspasija <aspasija.cvetkoska@result.si>
2024-07-04 13:46:03 +0200

    added notif icon

--- src/AdminDashboard/AdminConponents/SettingsComponents/SettingsComponents/NotificationsCard.jsx
+++ src/AdminDashboard/AdminConponents/SettingsComponents/SettingsComponents/NotificationsCard.jsx
@@ -1,5 +1,6 @@
 import EditIcon from "@mui/icons-material/Edit";
-import { Button } from "@mui/material";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import { Button, Container, IconButton } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -7,13 +8,26 @@ import CardHeader from "@mui/material/CardHeader";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormGroup from "@mui/material/FormGroup";
-import IconButton from "@mui/material/IconButton";
 import * as React from "react";
 
 const NotificationsCard = () => {
   return (
-    <Card sx={{ maxWidth: 1200 }}style={{paddingBottom:"15px", marginBottom:"20px"}}>
-      <CardHeader title="Notifications" subheader="Manage the notifications" />
+    <Card
+      sx={{ maxWidth: 1200 }}
+      style={{ paddingBottom: "15px", marginBottom: "20px" }}
+    >
+      <Container
+        style={{
+          display: "flex",
+          justifyContent: "start",
+          alignItems: "center",
+        }}
+      >
+        <IconButton aria-label="key" style={{ cursor: "auto" }}>
+          <NotificationsIcon sx={{ fontSize: 40 }} />
+        </IconButton>
+        <CardHeader title="Notifications" subheader="Manage notifications" />
+      </Container>
       <CardContent
         style={{
           marginLeft: "20px",
